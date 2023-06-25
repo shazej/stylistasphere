e17b7149ea0f6a30ebe2da261e5acbdf235bb6f7
shaz <shaz.e.j@gmail.com>
2023-06-25 15:59:21 +0300

    welcome page with logo

---
+++ backup/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/stylistasphere_logo.png',
+                width: 200,
+                height: 200,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to Stylistasphere',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
